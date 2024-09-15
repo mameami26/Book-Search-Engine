c48469752c5922da6536c57dcec01c6a91eca29b
mameami26 <mameami26@gmail.com>
2024-09-15 14:47:14 -0400

    auth.js and server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require ('@apollo/server');
+const { expressMiddleware } = require ('@apollo/server/express4');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const startAploserver = async () => {
+  await server.staet();
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/graphql', expressMiddleware(server));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +31,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startAploserver();
