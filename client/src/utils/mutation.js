dd581fb5aeda078faa820ad4e27a43b829dafe7e
mameami26 <mameami26@gmail.com>
2024-09-17 21:59:42 -0400

    Addded mutation.js file with its component

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,1 +1,23 @@
- 
+import { gql } from "@apollo/client";
+
+export const CREATE_USER = gql`
+  mutation createUser($book1: String!, $book2: String!) {
+    createUser(book1: $book1, book2: $book2) {
+      _id
+      book1
+      book2
+    }
+  }
+`;
+
+export const CREATE_VOTE = gql`
+  mutation createVote($_id: String!, $bookNum: Int!) {
+    creatVote(_id: $_id, bookNum: $bookNum) {
+      _id
+      book1
+      book2
+      book1_votes
+      book2_votes
+    }
+  }
+`;
