087e5b4ba5d45de4ec4f0660eea0bbc464a34ad6
mameami26 <mameami26@gmail.com>
2024-09-17 22:10:59 -0400

    Created a apollo Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloClient, ApolloProvider, InMemoryCach } from '@apollo/client';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCach(),
+});
 
 function App() {
   return (
     <>
+    <ApolloProvider client={client}>
+    <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Outlet />
+      </div>
+    </ApolloProvider>
       <Navbar />
       <Outlet />
     </>
