e7f30641454d1c40f24e8d7938a63c72b945f37c
mameami26 <mameami26@gmail.com>
2024-09-21 17:13:07 -0400

    change in the server

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 // Serve client/build as static assets if in production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 // Use routes
